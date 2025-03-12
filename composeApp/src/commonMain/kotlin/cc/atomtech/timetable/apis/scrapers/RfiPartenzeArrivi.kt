b45c1485f9b0d999e5199bf34d2e8d7f23b2f4aa
c4lopsitta <simone@atomtech.cc>
2025-03-12 16:15:41 +0100

    Implemented fetching departures and arrivals for a given station ID. Added `TrainData`, `TrainStopData`, `TrainDelayStatus`, `TrainStatus` and `TrainType` models.

--- composeApp/src/commonMain/kotlin/cc/atomtech/timetable/apis/scrapers/RfiPartenzeArrivi.kt
+++ composeApp/src/commonMain/kotlin/cc/atomtech/timetable/apis/scrapers/RfiPartenzeArrivi.kt
@@ -1,8 +1,12 @@
 package cc.atomtech.timetable.apis.scrapers
 
 import cc.atomtech.timetable.enumerations.StationCountry
+import cc.atomtech.timetable.enumerations.TrainType
 import cc.atomtech.timetable.models.rfi.StationBaseData
 import cc.atomtech.timetable.models.rfi.TrainData
+import cc.atomtech.timetable.models.rfi.TrainDelayStatus
+import cc.atomtech.timetable.models.rfi.TrainStatus
+import cc.atomtech.timetable.models.rfi.TrainStopData
 import cc.atomtech.timetable.scrapers.RfiScraper
 
 object RfiPartenzeArrivi {
@@ -15,10 +19,62 @@ object RfiPartenzeArrivi {
     }
 
     suspend fun getDepartures(stationId: Int) : List<TrainData> {
-        TODO()
+        val state = RfiScraper.getStationTimetable(stationId)
+        return List(state.departures.size) {
+            val train = state.departures[it]
+            TrainData(
+                operator = train.operator,
+                operatorName = "",
+                category = train.category,
+                categoryName = "",
+                number = train.number,
+                platform = train.platform,
+                delay = TrainDelayStatus(
+                    delay = train.delay,
+                    status = TrainStatus.RUNNING
+                ),
+                station = train.station,
+                time = train.time,
+                stops = List(train.stops.size) { index ->
+                    TrainStopData(
+                        name = train.stops[index].name,
+                        time = train.stops[index].time,
+                        isCurrentStop = train.stops[index].isCurrentStop
+                    )
+                },
+                details = train.details,
+                trainType = TrainType.DEPARTURE
+            )
+        }
     }
 
     suspend fun getArrivals(stationId: Int) : List<TrainData> {
-        TODO()
+        val state = RfiScraper.getStationTimetable(stationId)
+        return List(state.arrivals.size) {
+            val train = state.arrivals[it]
+            TrainData(
+                operator = train.operator,
+                operatorName = "",
+                category = train.category,
+                categoryName = "",
+                number = train.number,
+                platform = train.platform,
+                delay = TrainDelayStatus(
+                    delay = train.delay,
+                    status = TrainStatus.RUNNING
+                ),
+                station = train.station,
+                time = train.time,
+                stops = List(train.stops.size) { index ->
+                    TrainStopData(
+                        name = train.stops[index].name,
+                        time = train.stops[index].time,
+                        isCurrentStop = train.stops[index].isCurrentStop
+                    )
+                },
+                details = train.details,
+                trainType = TrainType.ARRIVAL
+            )
+        }
     }
 }
