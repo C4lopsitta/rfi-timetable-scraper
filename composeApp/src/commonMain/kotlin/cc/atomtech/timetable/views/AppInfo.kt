d65bbf664a83ed862eee322b40b7d9000679574a
srobaldo <srobaldo@elemento.cloud>
2026-02-20 18:27:30 +0100

    Added privacy policy reference inside App Info

--- composeApp/src/commonMain/kotlin/cc/atomtech/timetable/views/AppInfo.kt
+++ composeApp/src/commonMain/kotlin/cc/atomtech/timetable/views/AppInfo.kt
@@ -2,7 +2,9 @@ package cc.atomtech.timetable.views
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import cc.atomtech.timetable.StringRes
@@ -13,11 +15,17 @@ import cc.atomtech.timetable.StringRes
 @Composable
 fun AppInfo() {
     val versionNumber = "1.3.0"
+    val uriHandler = LocalUriHandler.current
 
     Column {
         Text(StringRes.get("app_name"), fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
         Text("cc.atomtech.timetable", fontSize = 8.sp, fontWeight = FontWeight.Light)
         Text("Application licensed under GNU GPLv3.")
         Text("Version ${StringRes.get("app_version")}")
+        TextButton({
+            uriHandler.openUri("https://raw.githubusercontent.com/C4lopsitta/rfi-timetable-scraper/refs/heads/main/privacy-policy")
+        }) {
+            Text("View Privacy Policy")
+        }
     }
 }
